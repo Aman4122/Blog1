05f026427ab90c587f3ecc3e655901d638996a9b
Aman Rajput <atirajput4122@gmail.com>
2023-04-19 11:30:04 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-// export const url = "https://creepy-calf-top-coat.cyclic.app";
-export const url = "http://localhost:8000";
+export const url = "https://blog1-production.up.railway.app";
+// export const url = "http://localhost:8000";
